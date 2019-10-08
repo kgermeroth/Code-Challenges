d4b8d38c2aa0fa5f9028230fba35880165fa9557
Kristin Germeroth <kristin.germeroth@gmail.com>
2019-10-07 17:00:31 -0700

    completed print-cursively

---
+++ print-recursively/printrecursively.py
@@ -0,0 +1,33 @@
+"""Print items in the list, using recursion.
+
+For example::
+
+   >>> print_recursively([1, 2, 3])
+   1
+   2
+   3
+
+"""
+
+
+def print_recursively(lst):
+    """Print items in the list, using recursion."""
+
+    # if not lst:
+    # 	return
+
+    # print(lst.pop(0))
+
+    # print_recursively(lst)
+
+    # alternate:
+    if lst:
+    	print(lst[0])
+    	print_recursively(lst[1:])
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TEST PASSED. GREAT JOB!\n")
